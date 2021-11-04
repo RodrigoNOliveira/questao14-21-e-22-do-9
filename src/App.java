5617369dec0efa78b325459c929c3230ee363af6
Rodrigo Nathan Oliveira <rodrigonathan4101@gmail.com>
2021-11-04 01:51:10 -0300

    codigo criado

---
+++ src/App.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class App {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int nmr, qtd;
+        int maior = 0;
+        int maiorS = 0;
+
+        int rep = 0;
+        System.out.print("Digite a quantidade de número que deseja inserir: ");
+        qtd = scanner.nextInt();
+
+        System.out.println("Digite números: ");
+
+        while (qtd > 0) {
+            nmr = scanner.nextInt();
+
+            if (nmr > maior) {
+                maior = nmr;
+                rep =1;
+                
+        }
+            else if (maior == nmr){
+                rep= rep+1;
+            }
+
+            qtd--;
+
+        }
+
+        System.out.println("O maior número digitado é:" + maior);
+        System.out.println("A quantidade de vezes que o maior número foi digitado foi de: " + rep);
+    }
+}
